b086a98b999f6ddb0c61cee856236680d1e8fb72
pirometheus <pirsu.turan@gmail.com>
2024-09-20 22:16:13 +0200

    I get RPS randomly without using func

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// let userEntry(prompt("Please enter a number!"))
-let random=Math.random();
-console.log(random);
+// function ranNum() {
+//     return Math.random();
+// }
+// console.log(ranNum());
+var ranNum=Math.random();
 
-function getComputeChoice(ranNum) {
-    return ranNum=Math.random();
-}
+console.log(ranNum);
 
-if (ranNum<0.33) {
+if (ranNum < 0.33) {
     console.log("Rock");
-} else if (0.33<=ranNum || ranNum<=0.66) {
+} else if (ranNum>= 0.33 && ranNum<=0.66) {
     console.log("Paper");
 } else {
     console.log("Scissor");
