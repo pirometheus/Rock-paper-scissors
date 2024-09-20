10d9b6b5b762121a689c0ac50837cb26e9d03c2e
pirometheus <pirsu.turan@gmail.com>
2024-09-20 22:23:15 +0200

    function has fixed

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-// function ranNum() {
-//     return Math.random();
-// }
-// console.log(ranNum());
-var ranNum=Math.random();
-
-console.log(ranNum);
-
-if (ranNum < 0.33) {
-    console.log("Rock");
-} else if (ranNum>= 0.33 && ranNum<=0.66) {
-    console.log("Paper");
-} else {
-    console.log("Scissor");
+function getComputerChoice(ranNum) {
+    if (ranNum < 0.33) {
+        console.log("Rock");
+    } else if (ranNum>= 0.33 && ranNum<=0.66) {
+        console.log("Paper");
+    } else {
+        console.log("Scissor");
+    }
 }
+
+console.log(getComputerChoice(Math.random()));
+
 
