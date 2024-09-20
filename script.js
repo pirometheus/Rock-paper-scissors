ecc8d02ffaa7a93f61fb47f8f03c3a76c632a2a6
pirometheus <pirsu.turan@gmail.com>
2024-09-21 00:17:16 +0200

    playRound written

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function getComputerChoice(ranNum) {
     if (ranNum < 0.33) {
-        console.log("Rock");
+        console.log("rock");
     } else if (ranNum>= 0.33 && ranNum<=0.66) {
-        console.log("Paper");
+        console.log("paper");
     } else {
-        console.log("Scissor");
+        console.log("scissor");
     }
 }
-
-console.log(getComputerChoice(Math.random()));
 
 let userChoice=prompt("Write rock, paper or scissor!");
 
@@ -17,3 +15,27 @@ function getHumenChoice() {
 }
 console.log(`User choise: ${userChoice}`)
 
+let humenScore=0;
+let computerScore=0;
+
+function playRound(humanChoice,computerChoice){
+    if (humanChoice==computerChoice) {
+        console.log("No one wins!");
+    } else if (humanChoice=="rock" && computerChoice=="scissor") {
+        console.log("You win! Rock beats Scissor");
+    } else if (humanChoice=="rock" && computerChoice=="paper") {
+        console.log("You lose! Paper beats Rock");
+    } else if (humanChoice=="scissor" && computerChoice=="paper") {
+        console.log("You win! Scissor beats Paper");
+    } else if (humanChoice=="scissor" && computerChoice=="rock") {
+        console.log("You lose! Rock beats Scissor");
+    }  else if (humanChoice=="paper" && computerChoice=="rock") {
+        console.log("You win! Paper beats Rock");
+    } else {
+        console.log("You lose! Scissor beats Paper");
+    }
+}
+const humanSelection=getHumenChoice(userChoice).toLowerCase();
+const computerSelection=getComputerChoice(Math.random);
+console.log(`Computer Choice: ${computerSelection}`);
+playRound(humanSelection,computerSelection);
